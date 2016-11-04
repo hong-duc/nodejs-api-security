aa1bf1f12832933a5750448eb0c9a0c1c03e152f
Minh Thành Nguyễn <Minh Thnh Nguyn>
2016-11-04 16:17:26 +0700

    Xong Nhánh login check session

--- src/api/routes/CheckSession.ts
+++ src/api/routes/CheckSession.ts
@@ -16,8 +16,6 @@ export class CheckSession {
         this.router.route('/')
             .get(this.Login)
             .post(this.Send)
-        this.router.route('/CheckSession')
-            .post(this.CheckSess)
         return this.router;
     }
 
@@ -27,14 +25,21 @@ export class CheckSession {
      */
     private Login = (req: Request, res: Response) => {
         console.log("GET")
+        console.log(req.session.id)
         Store.get(req.session.id, (err, sess) => {
             if (!err && !sess) {
                 if (req.cookies['UserName']) {
-                    return this.ReqToSever("http://localhost:4000/checkcache", req.cookies)
+                    return this.ReqToSever("http://localhost:4000/checkcache", this.SetDataRequest(req.cookies['UserName'], null, req, res))
+                        .then((result) => {
+                            res.cookie("UserName", result, { maxAge: 60 * 4 * 1000 })
+                                .redirect("../sign")
+                            return;
+                        })
+                        .catch(() => res.render("login"))
                 }
                 return res.render("login")
             }
-            return res.redirect(200, "../sign")
+            return res.redirect("../sign")
         })
     }
 
@@ -59,44 +64,44 @@ export class CheckSession {
         let r = request.post(sever, (err, response, body) => {
             if (!err && response.statusCode == 202) {
                 req.session.touch;
-                return this.SaveCookie(body, res)
+                //this.SaveCookie(body, res)
+                return res.send(200)
             }
 
             if (!err && response.statusCode == 200) {
-                console.info(body)
-                return this.SaveSession(req.session.cookie.maxAge, body['UserName'], req.session)
-                
+                return this.SaveSession(body['Session'], body['TTL'], req.session)
                     .then((result) => {
-                         this.SaveCookie(body, res)
+                        return this.SaveCookie(body, res)
                     })
+                    .catch(err => console.error(err))
             }
             if (!err && response.statusCode == 404) {
                 return res.render(view);
             }
 
-        }).json({
-            Cookie: this.SetCookie(req, res),
-            UserName: req.body.UserName,
-            PassHash: req.body.PassHash,
-            Session: req.session.id,
-            TTL: req.session.cookie.maxAge,
-            Server: req.protocol + '://' + req.get('host') + req.originalUrl
-        })
+        }).json(this.SetDataRequest(req.body.UserName, req.body.PassHash, req, res))
     }
 
     /**
      * Request to Server
      */
-    private ReqToSever = (Server: string, value: string) => {
-        let r = request.post(Server, (err, respone, body) => {
-            if (err) {
-                console.log(err)
-                return r.abort();
-            }
-            if (respone.statusCode == 200) {
-                return console.log(body)
-            }
-        }).json({ value })
+    private ReqToSever = (Server: string, value: {}) => {
+        return new Promise((resolve, reject) => {
+            let r = request.post(Server, (err, respone, body) => {
+                if (err) {
+                    console.log(err)
+                    r.abort();
+                    return reject(404)
+                }
+                if (respone.statusCode == 200) {
+                    this.DestroyOldSession(body['Session'])
+
+                    return resolve(body['UserName'])
+                }
+                if (respone.statusCode == 404)
+                    return reject(404)
+            }).json(value)
+        })
     }
 
     /**
@@ -104,7 +109,7 @@ export class CheckSession {
      */
     private SetCookie = (req: Request, res: Response) => {
         let c = new Cookie();
-        c.UserName = req.cookies['UserName'];
+
         c.IP = req.connection.remoteAddress;
         c.Brower = req.useragent.browser;
         c.Sever = req.protocol + '://' + req.get('host') + req.originalUrl
@@ -113,6 +118,17 @@ export class CheckSession {
         c.OS = req.useragent.os;
         return c;
     }
+    private SetDataRequest = (UserName: string, pass: string, req: Request, res: Response) => {
+        let values = {
+            Cookie: this.SetCookie(req, res),
+            UserName: UserName,
+            PassHash: pass,
+            Session: req.session.id,
+            TTL: req.session.cookie.maxAge,
+            Server: req.protocol + '://' + req.get('host') + req.originalUrl
+        };
+        return values;
+    }
 
     /**
      * Lưu session vào Store 
@@ -120,15 +136,13 @@ export class CheckSession {
      * TTL: time to life
      * UserName ,Brower,Sever
      */
-    private SaveSession = (TTL: number | string, UserName: string, sess: Express.Session) => {
+    private SaveSession = (sid: string, TTL: number | string, sess: Express.Session) => {
         return new Promise((resolve, reject) => {
-            session['UserName'] = UserName;
-            Store.set(UserName, sess, err => {
-                if (!err) {
-                    resolve("Luu session thanh cong")
-                } else {
-                    reject("Faile cmnr")
-                }
+
+            Store.set(sid, sess, err => {
+                if (err)
+                    reject("Faile cmnr");
+                resolve("Luu session thanh cong");
             })
         })
     }
@@ -136,13 +150,21 @@ export class CheckSession {
      * , httpOnly: true thì nó sẽ disable javascript trên trình duyệt document.cookie
      */
     private SaveCookie = (Value: {}, res: Response) => {
+        // maxAge: Value['TTL']
         res
-            .cookie('UserName', Value['UserName'], { maxAge: Value['TTL'] })
-            .redirect(200, "../sign")
+            .cookie('UserName', Value['UserName'], { maxAge: 60 * 4 * 1000 })
+            .redirect("../sign")
     }
 
-    private CheckSess = (req: Request, res: Response) => {
-        console.log(req.body)
+    private DestroyOldSession = (sid: string) => {
+        Store.all((err, obj) => {
+            console.log(obj)
+        })
+        console.log(`SID: ${sid}`)
+        Store.get(sid, (err, sess) => {
+            console.log(sess)
+        })
+        Store.destroy(sid, er => { })
     }
 
 }
